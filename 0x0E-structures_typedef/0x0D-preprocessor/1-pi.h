f323d760f10a399df6ffdfc2426e2167d51f7330
Ficharagu <wanjiru.fionah@gmail.com>
2022-04-14 13:34:04 +0300

    kio

--- 0x0E-structures_typedef/0x0D-preprocessor/1-pi.h
+++
@@ -1,4 +0,0 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-#define PI 3.14159265359
-#endif
--- 0x0E-structures_typedef/0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef ABS_HEADER_FILE
-#define ABS_HEADER_FILE
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
-#endif
